80341fbc5e5ef92708530b636b1e9ba5392a25b1
Abdulkadir Aktar <aktarkadir1@gmail.com>
2024-03-30 21:11:26 +0300

    Revision

--- KullaniciEtkilesimi/ViewController.swift
+++ KullaniciEtkilesimi/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonAlert(_ sender: Any) {
+        let alertController = UIAlertController(title: "UYARI", message: "Uyarı Mesajı", preferredStyle: .alert)
+                let cancelAction = UIAlertAction(title: "iptal", style: .cancel){
+                    action in print("iptal seçildi")
+                }
+                alertController.addAction(cancelAction)
+                let okAction = UIAlertAction(title: "Tamam", style: .destructive){
+                    action in print("tamam seçildi")
+                }
+                alertController.addAction(okAction)
+                    self.present(alertController, animated: true)
+    }
+    
+    @IBAction func buttonActionSheet(_ sender: Any) {
+        let alertController = UIAlertController(title: "UYARI", message: "Uyarı Mesajı", preferredStyle: .actionSheet)
+                    let cancelAction = UIAlertAction(title: "iptal", style: .cancel){
+                        action in print("iptal seçildi")
+                    }
+                    alertController.addAction(cancelAction)
+                    let okAction = UIAlertAction(title: "Tamam", style: .destructive){
+                        action in print("tamam seçildi")
+                    }
+                    alertController.addAction(okAction)
+                        self.present(alertController, animated: true)
+    }
+    
 }
 
